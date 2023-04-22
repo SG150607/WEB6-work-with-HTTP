aaa9cb55b5b5d50c43ce3a70ce085ba5abb7d475
Sabina Gasymova <SabinaGasymova150607@gmail.com>
2023-04-22 17:02:22 +0300

    classwork3

--- main.py
+++ main.py
@@ -23,19 +23,13 @@ if not response:
     # обработка ошибочной ситуации
     pass
 
-# Преобразуем ответ в json-объект
 json_response = response.json()
-# Получаем первый топоним из ответа геокодера.
-toponym = json_response["response"]["GeoObjectCollection"][
-    "featureMember"][0]["GeoObject"]
-# Координаты центра топонима:
+toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
 toponym_coodrinates = toponym["Point"]["pos"]
-# Долгота и широта:
 toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
 
 delta = "0.005"
 
-# Собираем параметры для запроса к StaticMapsAPI:
 map_params = {
     "ll": ",".join([toponym_longitude, toponym_lattitude]),
     "spn": ",".join([delta, delta]),
@@ -43,10 +37,6 @@ map_params = {
 }
 
 map_api_server = "http://static-maps.yandex.ru/1.x/"
-# ... и выполняем запрос
 response = requests.get(map_api_server, params=map_params)
 
-Image.open(BytesIO(
-    response.content)).show()
-# Создадим картинку
-# и тут же ее покажем встроенным просмотрщиком операционной системы
+print(response.url)
--- the_file.py
+++ the_file.py
@@ -1,7 +1,5 @@
 import requests
 import sys
-from io import BytesIO
-from PIL import Image
 from get_coordinates import get_coordinates
 
 search_api_server = "https://search-maps.yandex.ru/v1/"
@@ -21,7 +19,6 @@ search_params = {
 
 response = requests.get(search_api_server, params=search_params)
 if not response:
-    # ...
     pass
 
 json_response = response.json()
@@ -34,14 +31,25 @@ point = organization["geometry"]["coordinates"]
 org_point = "{0},{1}".format(point[0], point[1])
 
 map_params = {
-    "ll": address_ll,
+    "ll": "{0},{1}".format((float(address_ll.split(",")[0]) + float(org_point.split(",")[0])) / 2,
+                           (float(address_ll.split(",")[1]) + float(org_point.split(",")[1])) / 2),
     "spn": spn,
     "l": "map",
-    # добавим точку, чтобы указать найденную аптеку
-    "pt": "{0},pm2dgl".format(org_point)
+    "pt": "{0},pm2dgl~{1},home".format(org_point, address_ll)
 }
 
 map_api_server = "http://static-maps.yandex.ru/1.x/"
 response = requests.get(map_api_server, params=map_params)
 
-Image.open(BytesIO(response.content)).show()
+print(response.url)
+
+info_to_print = {"Адрес": org_address, "Название аптеки": org_name,
+                 "Время работы": organization["properties"]["CompanyMetaData"]["Hours"]["text"],
+                 "Расстояние до аптеки": (abs(float(address_ll.split(",")[0]) - float(org_point.split(",")[0])),
+                                          abs(float(address_ll.split(",")[1]) - float(org_point.split(",")[1])))}
+
+print(f'Адрес: {info_to_print["Адрес"]}',
+      f'Название аптеки: {info_to_print["Название аптеки"]}',
+      f'Время работы: {info_to_print["Время работы"]}',
+      f'Расстояние до аптеки: {info_to_print["Расстояние до аптеки"]}',
+      sep="\n")
